f79020e78aa2494a4b7a0edc6ba33e0e10e43889
davidlovescode <david.aotieno@strathmore.edu>
2026-08-05 09:57:45 +0300

    yezaaaa

--- OneDrive/Documents/30daysofpython/helloworld.py
+++ OneDrive/Documents/30daysofpython/helloworld.py
@@ -1,19 +1,30 @@
 # Day 1 - 30DaysOfPython Challenge
 
-print(2 + 3)             # addition(+)
-print(3 - 1)             # subtraction(-)
-print(2 * 3)             # multiplication(*)
-print(3 / 2)             # division(/)
-print(3 ** 2)            # exponential(**)
-print(3 % 2)             # modulus(%)
-print(3 // 2)            # Floor division operator(//)
+#checking python type
+import sys
+print(sys.version)
+
+#operations
+print(3 + 4)             # addition(+)
+print(3 - 4)             # subtraction(-)
+print(3 * 4)             # multiplication(*)
+print(3 % 4)             # modulus(%)
+print(3 / 4)             # division(/)
+print(3 ** 4)            # exponential(**)
+print(3 // 4)            # Floor division operator(//)
+
+# Writing string
+print('David')
+print('Abel')
+print('Kenya')
+print('I am enjoying 30 days of python')
 
 # Checking data types
 print(type(10))          # Int
-print(type(3.14))        # Float
-print(type(1 + 3j))      # Complex number
-print(type('Asabeneh'))  # String
-print(type([1, 2, 3]))   # List
-print(type({'name':'Asabeneh'})) # Dictionary
-print(type({9.8, 3.14, 2.7}))    # Set
-print(type((9.8, 3.14, 2.7)))    # Tuple
+print(type(9.8))        # Float
+print(type(3.14))      #float
+print(type(['Asabeneh', 'Python', 'Finland']))  # List
+print(type('David'))   # string
+print(type('Abel')) # string
+print(type('Kenya'))    # String
+
